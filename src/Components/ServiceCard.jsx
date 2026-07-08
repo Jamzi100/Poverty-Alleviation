f428ff5b495d00cebd96dcf97511b4abd03ff5b7
Daniel Joseph <danieljosephdj111@gmail.com>
2026-07-08 15:19:45 -0700

    next commit

--- src/Components/ServiceCard.jsx
+++ src/Components/ServiceCard.jsx
@@ -1,12 +1,12 @@
 export default function ServiceCard({ image, title, description}) {
     return(
-        <div>
-            <img src={Image} alt={title} className="w-full h-56 object-cover" />
+        <div className="max-w-lg rounded overflow-hidden shadow-lg bg-white mx-auto mb-8">
+            <img src={image} alt={title} className="w-100 h-70 object-cover rounded-2xl p-4" />
 
-             <div className="p-6">
+             <div className="p-4">
         <h3 className="text-3xl font-bold text-red-800">{title}</h3>
 
-        <p className="mt-4 text-gray-600">
+        <p className="mt-4 text-gray-600 text-2xl">
           {description}
         </p>
       </div>
